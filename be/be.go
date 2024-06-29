2583ee1b5b6ef084cf263730e9a4e15683564b8b
Graffioh <bregliascuola2002@gmail.com>
2024-06-29 20:01:43 +0200

    Add port number in response string

--- be/be.go
+++ be/be.go
@@ -7,14 +7,16 @@ import (
 	"sync"
 )
 
-func hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "hello from backend server")
+func hello(w http.ResponseWriter, r *http.Request, port int) {
+	fmt.Fprintf(w, "Hello from backend server on port %d", port)
 }
 
 func createServer(port int) *http.Server {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/", hello)
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		hello(w, r, port)
+	})
 
 	server := http.Server{
 		Addr:    fmt.Sprintf(":%v", port),
@@ -30,26 +32,13 @@ func main() {
 
 	log.Println("backend servers started")
 
-	go func() {
-		server := createServer(8080)
-		server.ListenAndServe()
-
-		wg.Done()
-	}()
-
-	go func() {
-		server := createServer(8081)
-		server.ListenAndServe()
-
-		wg.Done()
-	}()
-
-	go func() {
-		server := createServer(8082)
-		server.ListenAndServe()
-
-		wg.Done()
-	}()
+	for _, port := range []int{8080, 8081, 8082} {
+		go func(p int) {
+			server := createServer(p)
+			server.ListenAndServe()
+			wg.Done()
+		}(port)
+	}
 
 	wg.Wait()
 }
